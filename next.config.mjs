511859d517d22d6fed6623b3c1a11701dafdf761
MnVOIDnM <gg.1586yterkfu@gmail.com>
2024-08-18 23:57:20 +0900

    setup husky, lint, lint-staged

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,22 @@
-import { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } from 'next/dist/shared/lib/constants.js';
+import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants.js";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
     return [
-      // Basic redirect
       {
-        source: '/',
-        destination: '/notes',
+        destination: "/notes",
         permanent: true,
+        source: "/",
       },
-      // Wildcard path matching
-      // {
-      //   source: '/blog/:slug',
-      //   destination: '/news/:slug',
-      //   permanent: true,
-      // },
     ];
   },
 };
 
 const nextConfigFunction = async (phase) => {
   if (phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = (await import('next-pwa')).default({
-      dest: 'public',
+    const withPWA = (await import("next-pwa")).default({
+      dest: "public",
     });
     return withPWA(nextConfig);
   }
